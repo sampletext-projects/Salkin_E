1e9aa09b5d1129371a28c3dbcb8f8370febf0e17
sampletext32 <sampletext32@bk.ru>
2020-07-09 13:16:29 +0300

    Add project

---
+++ Salkin_E/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salkin_E
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = 0;
+            while (s.Length > 0)
+            {
+                int nextSpace = s.IndexOf(' ');
+                string word = s.Substring(0, nextSpace);
+                if (word[0] == word[word.Length - 1])
+                {
+                    n++;
+                }
+
+                s = s.Remove(0, nextSpace + 1);
+            }
+
+            Console.WriteLine("Слов: {0}", n);
+
+            Console.ReadKey();
+        }
+    }
+}
